1a52886db6defba0b11444830b7c9bae63616444
Creesix <remipbw@gmail.com>
2024-11-12 08:05:01 +0100

    TP1-Question4

---
+++ TP04.m
@@ -0,0 +1,63 @@
+% Resolution equation de Laplace
+%
+clear
+close all
+clc
+
+%% Dimensions / maillage
+dx=1; % cm
+dy=1; % cm
+Nx = 80;
+Ny = 80;
+
+%% Potentiels / sources
+v0 = 0; % condition aux limites (en V)
+v1 = 100; % conducteur 1
+v2 = -100; % conducteur 2
+
+% Initialisation la matrice de calcul
+V = zeros(Nx,Ny); % mettre toute la matrice a zero
+Vold=zeros(Nx,Ny);
+N=1;
+diff=v1;
+seuil=0.001;
+
+%Condition aux limites 
+V(1,1:Ny)=v0;
+V(Nx,1:Ny)=v0;
+V(1:Nx,1)=v0;
+V(1:Nx,Ny)=v0;
+%sources
+V(26:29,8:34)=v1;
+V(5:22,20:21)=v2;
+
+
+%% Equation de calcul
+
+while diff > seuil
+    N=N+1;
+    i=2:Nx-1;
+    j=2:Ny-1;
+    Vold=V;
+    V(i,j)=0.25*( V(i+1,j) + V(i-1,j) + V(i,j+1) + V(i,j-1) );
+       
+    %Condition aux limites 
+    V(1,1:Ny)=v0;
+    V(Nx,1:Ny)=v0;
+    V(1:Nx,1)=v0;
+    V(1:Nx,Ny)=v0;
+    %sources
+    V(26:29,8:34)=v1;
+    V(5:22,20:21)=v2;
+    %diff=max(max(Vold-V));
+    diff= norm(abs(Vold-V)); %énormément de changement au niveau du nombre d'itération en utilisant la norme au lieu du max
+end
+
+
+
+
+%% Figure
+figure(1)
+colormap(jet);
+pcolor(V);
+title("le nombre d'itération est :", N)
